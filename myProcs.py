edee5a7620d8fed60641f6e59c84c49cccfbaadf
andreygal <andreygalper@gmail.com>
2017-09-29 20:54:10 -0400

    Create myProcs.py

---
+++ myProcs.py
@@ -0,0 +1,16 @@
+def centerPts(compSet):
+	pts = [compSet]
+	center = pts[0] 
+	for i in (1, len(compSet))
+	 pt1 = center
+	 pt2 = pts[i]
+	 center = (pt1 - pt2) / 2
+	centSet = set()
+	while (compSet)
+     transPt = compSet.pop()
+     transPt.real = transPt.real - center.real
+     transPt.imag = transPt.imag - center.imag
+	 centSet.add(newPt)
+return centSet
+
+
