59a7f958b13785785ac85d57e2acec251d97939b
Andrey Galper <andreygalper@gmail.com>
2017-10-04 16:27:27 -0400

    Refactored

--- myProcs.py
+++ myProcs.py
@@ -1,16 +1,16 @@
-def centerPts(compSet):
- pts = [compSet]
- center = pts[0] 
- for i in (1, len(compSet))
-  pt1 = center
-  pt2 = pts[i]
-  center = (pt1 - pt2) / 2
- centSet = set()
- while (compSet)
-  transPt = compSet.pop()
-  transPt.real = transPt.real - center.real
-  transPt.imag = transPt.imag - center.imag
-  centSet.add(newPt)
-return centSet
+def center_pts(comp_set):
+    pts = [comp_set]
+    center = pts[0]
+    for i in (1, len(comp_set)):
+        pt1 = center
+        pt2 = pts[i]
+        center = (pt1 - pt2) / 2
+        cent_set = set()
+    while comp_set:
+        trans_pt = comp_set.pop()
+        trans_pt.real = trans_pt.real - center.real
+        trans_pt.imag = trans_pt.imag - center.imag
+        cent_set.add(trans_pt)
+    return cent_set
 
 
